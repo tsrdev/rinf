7e464e03caae1b8b516e9aa42651abcda86d835c
Alex P <dtk077@gmail.com>
2014-05-13 23:57:20 +0300

    Update homepage path

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,6 +14,7 @@ angular
     $locationProvider.html5Mode(true).hashPrefix('!');
     $routeProvider
       .when('/', {
+        title: "Home",
         templateUrl: '/views/main.html',
         controller: 'MainCtrl'
       })
